7bad423ba3d3037942c1a907998501567d916fa0
ADDGames <aikodedoncker@hotmail.com>
2018-04-30 12:54:32 +0200

    x

---
+++ VanMoer-WebFrontService/src/main/java/be/odisee/vanmoer/service/WebFrontServiceImpl.java
@@ -0,0 +1,18 @@
+package be.odisee.vanmoer.service;
+
+import org.springframework.stereotype.Service;
+
+import be.odisee.vanmoer.domain.Klant;
+
+@Service
+public class WebFrontServiceImpl implements WebFrontService {
+
+	@Override
+	public Klant setId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
