c1a297d740bf09f330d11137e701274ff75a122a
ADDGames <aikodedoncker@hotmail.com>
2018-05-14 03:25:48 +0200

    one to many

--- VanMoer-RestService/src/main/java/be/odisee/vanmoer/domain/Locatie.java
+++ VanMoer-RestService/src/main/java/be/odisee/vanmoer/domain/Locatie.java
@@ -6,9 +6,11 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -26,6 +28,7 @@ public class Locatie {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
+	@OneToMany(targetEntity=Product.class, mappedBy="locatie", fetch=FetchType.EAGER)
 	@Column(name="producten")
 	private List<Product> producten;
 	@Column(name="status")
--- VanMoer-TransportService/src/main/java/be/odisee/vanmoer/domain/Transport.java
+++ VanMoer-TransportService/src/main/java/be/odisee/vanmoer/domain/Transport.java
@@ -3,11 +3,14 @@
 package be.odisee.vanmoer.domain;
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
